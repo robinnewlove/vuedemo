3e0d9dbf7613a9d5119ee05f3a6e0c48ab5f9b31
Robin <robinnewlove@gmail.com>
2016-12-21 14:21:05 +0800

    修改

--- src/components/newslist.vue
+++ src/components/newslist.vue
@@ -1,38 +1,29 @@
 <template>
     <div>
         <ul>
-            <li v-for="item in listData.list">{{item.title}}</li>
+        <li v-for="item in listData.list">{{item.title}}</li>
         </ul>
     </div>
 </template>
 <script>
 import axios from 'axios';
 export default {
-    data ()
-{
-    return {
-        //gridColumns: ['1','2','3','4'],
-        listData: [],
-        //apiUrl: 'http://v.juhe.cn/weixin/query?key=59803c8d89adc0df0313b4c863edefb6'
-        apiUrl: 'http://127.0.0.1/vuedemo/vuedemo/data.php'
+    data () {
+        return {
+            listData: [],
+            apiUrl: 'http://127.0.0.1/vuedemo/vuedemo/data.php'
 
-    }
-}
-,
-created:function () {
-    axios.get(this.apiUrl).then((response) = > {
-        //return response;
-        this.listData = response.data.result;
-    //console.log(this.listData);
-
-    //return this.gridColumns
-}
-)
-.catch(function (error) {
+        }
+    },
+    created:function(){
+         axios.get(this.apiUrl).then((response) => {
+    this.listData = response.data.result;
+  })
+  .catch(function (error) {
     console.log(error);
-});
-
-
-}
+  });
+        
+        
+    }
 }
 </script>
